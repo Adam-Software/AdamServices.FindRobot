ad6d6f6159c298ebe9d25db50c73693b425b96ac
Alexey Nesterov <vertigra.developer@gmail.com>
2025-02-04 10:27:13 +1000

    Добавьте файлы проекта.

---
+++ src/Services.FindRobot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Services.FindRobot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
