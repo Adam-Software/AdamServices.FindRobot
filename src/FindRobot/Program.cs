3aef404a7a4add5084f17bc8fd020f3c9e1d5198
Alexey Nesterov <vertigra.developer@gmail.com>
2025-03-11 09:13:43 +1000

    Update Program.cs

--- src/FindRobot/Program.cs
+++ src/FindRobot/Program.cs
@@ -12,7 +12,7 @@ namespace FindRobot
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             IHost host = Host.CreateDefaultBuilder(args)
                     .ConfigureAppConfiguration((context, config) =>
